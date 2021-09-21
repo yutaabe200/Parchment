35561f76ffd81adda3db9fc937c3f3389f976c85
yutaabe200 <yutaabe200@gmail.com>
2021-09-21 15:32:34 +0900

    fix ios version

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "Parchment",
-    // platforms: [.iOS("9.0")],
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13)
-    ],
+    platforms: [.iOS("13.0")],
+    // platforms: [
+    //     .iOS("13.0"),
+    //     .macOS(.v10_15),
+    //     .tvOS(.v13)
+    // ],
     products: [
         .library(name: "Parchment", targets: ["Parchment"]),
     ],
